b2dc0338b377f5ab5a595493e512915be9600793
geistman <geistman@gmail.com>
2016-01-31 10:31:05 +0100

    splitted declaration and tests

--- SpringRestTutorial/src/test/java/SimpleRestClient/SimpleRestClientTest.java
+++ SpringRestTutorial/src/test/java/SimpleRestClient/SimpleRestClientTest.java
@@ -14,22 +14,23 @@ public class SimpleRestClientTest {
 
     private static final Logger log = LoggerFactory.getLogger(SimpleRestClientTest.class);
     private static RestTemplate restTemplate = new RestTemplate();
-    private  static final Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+    private  static final Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/4", Quote.class);
 
     @org.junit.BeforeClass
     public static void setUp() throws Exception {
     }
 
     @Test
-    public void testQuoteSuccess() throws Exception {
-
-        log.info("String representation of quote: " + quote.toString());
+    public void testQuoteInitialized() throws Exception {
+        log.debug("String representation of quote: " + quote.toString());
         assertNotNull(quote.getType());
         assertNotNull(quote.getValue());
+    }
 
-        log.info("Type: " + quote.getType());
+    @Test
+    public void testQuoteSuccess() throws Exception {
+        log.debug("Type: " + quote.getType());
         assertEquals(quote.getType(), "success");
-
     }
 
 }
