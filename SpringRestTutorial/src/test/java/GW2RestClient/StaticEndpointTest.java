301e9c0e45e960099bc765149cfa1420bbb414f4
geistman <geistman@gmail.com>
2016-02-13 22:48:34 +0100

    refactored to hamcrest test and removed google tests. GW2 API Test only now

--- SpringRestTutorial/src/test/java/GW2RestClient/StaticEndpointTest.java
+++ SpringRestTutorial/src/test/java/GW2RestClient/StaticEndpointTest.java
@@ -5,6 +5,8 @@ import org.junit.runners.model.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static java.lang.Boolean.*;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StaticEndpointTest {
@@ -12,21 +14,9 @@ public class StaticEndpointTest {
     private final static Logger log = LoggerFactory.getLogger(StaticEndpointTest.class);
 
     @Test
-    public void testHttpIsReachable() throws Exception {
-        StaticEndpoint staticEndpoint = new StaticEndpoint("http://www.google.com");
-        assertTrue(staticEndpoint.isReachable());
-    }
-
-    @Test
-    public void testHttpsIsReachable() throws Exception {
-        StaticEndpoint staticEndpoint = new StaticEndpoint("https://www.google.com");
-        assertTrue(staticEndpoint.isReachable());
-    }
-
-    @Test
     public void testGW2ApiIsReachable() throws Exception {
         StaticEndpoint staticEndpoint = new StaticEndpoint("https://api.guildwars2.com");
-        assertTrue(staticEndpoint.isReachable());
+        assertThat(staticEndpoint.isReachable(), is(TRUE));
     }
 
 }
