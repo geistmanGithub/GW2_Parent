ba302bf0034f459c6e30bd257a9f95846c95a21d
geistman <geistman@gmail.com>
2016-02-13 21:25:38 +0100

    switched logger to slf4j

--- SpringRestTutorial/src/main/java/GW2RestClient/StaticEndpoint.java
+++ SpringRestTutorial/src/main/java/GW2RestClient/StaticEndpoint.java
@@ -1,24 +1,35 @@
 package GW2RestClient;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+
 
 public class StaticEndpoint {
 
+    private static final Logger log = LoggerFactory.getLogger(StaticEndpoint.class);
     protected boolean isReachable() {
 
 
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL("http://www.google.com").openConnection();
+            final URL url = new URL("http://www.google.com");
+            final URLConnection urlConnection = url.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) urlConnection;
             connection.setConnectTimeout(3000);
             connection.setReadTimeout(3000);
             connection.setRequestMethod("HEAD");
             int responseCode = connection.getResponseCode();
+            log.debug("URL:" + url.toExternalForm() + " Response Code:" + responseCode);
             return (200 <= responseCode && responseCode <= 399);
 
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(e.toString(), e);
             return false;
         }
 
--- SpringRestTutorial/src/test/java/GW2RestClient/StaticEndpointTest.java
+++ SpringRestTutorial/src/test/java/GW2RestClient/StaticEndpointTest.java
@@ -1,13 +1,20 @@
 package GW2RestClient;
 
 import org.junit.Test;
+import org.junit.runners.model.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.*;
 
 public class StaticEndpointTest {
 
+    private final static Logger log = LoggerFactory.getLogger(StaticEndpointTest.class);
+
     @Test
     public void testIsReachable() throws Exception {
-        assertTrue(new StaticEndpoint().isReachable());
+        StaticEndpoint staticEndpoint = new StaticEndpoint();
+
+        assertTrue(staticEndpoint.isReachable());
     }
 }
