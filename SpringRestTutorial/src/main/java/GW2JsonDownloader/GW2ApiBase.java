7cbd105068d9a7d522dc32966acb6b64a3c458af
geistman <geistman@gmail.com>
2016-02-14 22:20:58 +0100

    renamed to API Base, because there is no JSON there yet. It's not the endpoint yet. test for expected versions of GW2 API. refactored and split HTTP Connection for APIBase

---
+++ SpringRestTutorial/src/main/java/GW2JsonDownloader/GW2ApiBase.java
@@ -0,0 +1,71 @@
+package GW2JsonDownloader;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+
+
+public class GW2ApiBase {
+
+    private static final Logger log = LoggerFactory.getLogger(GW2ApiBase.class);
+    private URL url;
+
+
+    public GW2ApiBase(String url) {
+
+        try {
+            this.url = new URL(url);
+        } catch (MalformedURLException e) {
+            log.error(e.toString(), e);
+        }
+    }
+
+
+    protected boolean isReachable() {
+        int responseCode = 0;
+        try {
+            HttpURLConnection connection = getHttpURLConnection();
+            responseCode = connection.getResponseCode();
+        } catch (IOException e) {
+            log.error(e.toString(), e);
+        }
+
+        log.debug("URL:" + url.toExternalForm() + " Response Code:" + responseCode);
+            return (200 <= responseCode && responseCode <= 399);
+    }
+
+    private HttpURLConnection getHttpURLConnection() throws IOException{
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setConnectTimeout(3000);
+        connection.setReadTimeout(3000);
+        connection.setRequestMethod("GET");
+
+        return connection;
+    }
+
+    protected String getContent() {
+        StringBuilder result = new StringBuilder();
+        try {
+            HttpURLConnection connection = getHttpURLConnection();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            result.append(reader.readLine());
+            reader.close();
+            log.debug("Content:" + result.toString());
+
+        } catch (IOException e) {
+            log.error(e.toString(), e);
+            return null;
+        }
+        return result.toString();
+
+
+    }
+}
---
+++ SpringRestTutorial/src/test/java/GW2JsonDownloader/GW2ApiBaseTest.java
@@ -0,0 +1,27 @@
+package GW2JsonDownloader;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.Boolean.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class GW2ApiBaseTest {
+
+    private final static Logger log = LoggerFactory.getLogger(GW2ApiBaseTest.class);
+    private final String VERSIONENDPOINTSTRING = "[\"v1\",\"v2\"]";
+
+    @Test
+    public void testGW2ApiIsReachable() throws Exception {
+        GW2ApiBase gw2ApiBase = new GW2ApiBase("https://api.guildwars2.com");
+        assertThat(gw2ApiBase.isReachable(), is(TRUE));
+    }
+
+    @Test
+    public void testAPIVersionIsAsExpected() throws Exception {
+        GW2ApiBase gw2ApiBase = new GW2ApiBase("https://api.guildwars2.com");
+        assertThat(gw2ApiBase.getContent(), is(VERSIONENDPOINTSTRING));
+    }
+}
